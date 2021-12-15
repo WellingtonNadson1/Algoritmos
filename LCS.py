36de218c5bdb36380a682e60e2b6d05991ad7d12
Wellington Nadson <76890847+WellingtonNadson1@users.noreply.github.com>
2021-12-15 15:23:58 -0300

    Subsequência Comum Mais Longa
    Estudo do algoritmo Longest Common Subsequence, ou seja, Subsequência Comum Mais Longa.

---
+++ LCS.py
@@ -0,0 +1,20 @@
+# Longest Common Subsequence
+
+def lcs(X, Y):
+    n = len(X)
+    m = len(Y)
+    l = {}
+    
+    for i in range(n + 1):
+        l[(i, 0)] = 0
+    
+    for j in range(m + 1):
+        l[(0, j)] = 0
+        
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if X[i - 1] == Y[j - 1]:
+                l[(i, j)] = l[(i - 1, j - 1)] + 1
+            else:
+                l[(i, j)] = max(l[(i - 1, j)], l[(i, j - 1)])
+                
